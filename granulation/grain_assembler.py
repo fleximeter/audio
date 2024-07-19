d626f3faf334123a265d15b273a92f8ce46e83fd
Jeff Martin <jeffreymartin@outlook.com>
2024-07-19 11:38:58 -0500

    overhauled grain overlapper

--- granulation/grain_assembler.py
+++ granulation/grain_assembler.py
@@ -7,44 +7,34 @@ Description: Contains grain assembler tools
 import aus.operations as operations
 import numpy as np
 import random
-from effects import *
 
 
-def assemble_single(grains: list, features: list, interval: int, max_db: float = -18.0) -> np.ndarray:
+def assemble_single(grains: list, features: list, interval: int, max_db: float = -18.0, window_fn = np.hanning, effect_chain: list = None, effect_cycle: list = None) -> np.ndarray:
     """
     Assembles grains given specified parameters. Applies a window norm to the final assembled grains.
     :param grains: A list of grain dictionaries to choose from
     :param feature: The string name of the audio feature to use
     :param interval: The interval between each grain, in frames
     :param max_db: The db level of the final grains
+    :param window_fn: A window function to apply to all grains
+    :param effect_chain: A sequence (list) of effects that will be applied to each grain individually. If None, no effects will be applied.
+    :param effect_cycle: A cycle (list) of effects that will be applied to each grain in a repeating sequence. If None, no effects will be applied.
     :return: The assembled grains as an array
     """
-    effect_chain = [
-        ButterworthFilterEffect(50, "highpass", 4)
-    ]
-    effect_cycle = [
-        IdentityEffect(), 
-        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
-        IdentityEffect(), 
-        ButterworthFilterEffect(440, "lowpass", 2),
-        IdentityEffect(), 
-        ButterworthFilterEffect(440, "lowpass", 2),
-        IdentityEffect(), 
-        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
-    ]
-
     # Organize the grains
     for feature in features:
         grains = sorted(grains, key=lambda x: x[feature])
 
     # Create the window and initialize the window norm
-    window = np.hanning(grains[0]["length"])
+    window = window_fn(grains[0]["length"])
     window_norm = window.copy()
 
     # Apply effects
-    for effect in effect_chain:
-        grains[0]["grain"] = effect(grains[0]["grain"])
-    grains[0]["grain"] = effect_cycle[0](grains[0]["grain"])
+    if effect_chain is not None:
+        for effect in effect_chain:
+            grains[0]["grain"] = effect(grains[0]["grain"])
+    if effect_cycle is not None:
+        grains[0]["grain"] = effect_cycle[0](grains[0]["grain"])
 
     # Add the grain to the audio sequence
     grains[0]["grain"] = operations.adjust_level(grains[0]["grain"], max_db)
@@ -52,11 +42,13 @@ def assemble_single(grains: list, features: list, interval: int, max_db: float =
     last_grain_onset = 0
     for i in range(1, len(grains)):
         # Apply effects
-        window = np.hanning(grains[i]["length"])
-        for effect in effect_chain:
-            grains[i]["grain"] = effect(grains[i]["grain"])
-        grains[i]["grain"] = effect_cycle[i % len(effect_cycle)](grains[i]["grain"])
+        if effect_chain is not None:
+            for effect in effect_chain:
+                grains[i]["grain"] = effect(grains[i]["grain"])
+        if effect_cycle is not None:
+            grains[i]["grain"] = effect_cycle[i % len(effect_cycle)](grains[i]["grain"])
         grains[i]["grain"] = operations.adjust_level(grains[i]["grain"], max_db)
+        window = window_fn(grains[i]["length"])
         grains[i]["grain"] *= window
 
         # Add the grain to the audio sequence and update the window norm
@@ -71,7 +63,7 @@ def assemble_single(grains: list, features: list, interval: int, max_db: float =
     return final_audio
 
 
-def assemble_stochastic(grains: list, n: int, interval: int, rng: random.Random, max_db: float =-18.0) -> np.ndarray:
+def assemble_stochastic(grains: list, n: int, interval: int, rng: random.Random, max_db: float =-18.0, window_fn = np.hanning, effect_chain: list = None, effect_cycle: list = None) -> np.ndarray:
     """
     Assembles grains stochastically. Each grain is used n times. Applies a window norm to the final assembled grains.
     :param grains: A list of grain dictionaries to choose from
@@ -79,24 +71,13 @@ def assemble_stochastic(grains: list, n: int, interval: int, rng: random.Random,
     :param rng: A random number generator object
     :param interval: The interval between each grain, in frames
     :param max_db: The db level of the final grains
+    :param window_fn: A window function to apply to all grains
+    :param effect_chain: A sequence (list) of effects that will be applied to each grain individually. If None, no effects will be applied.
+    :param effect_cycle: A cycle (list) of effects that will be applied to each grain in a repeating sequence. If None, no effects will be applied.
     :return: The assembled grains as an array
     """
-    effect_chain = [
-        ButterworthFilterEffect(50, "highpass", 4)
-    ]
-    effect_cycle = [
-        IdentityEffect(), 
-        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
-        IdentityEffect(), 
-        ButterworthFilterEffect(440, "lowpass", 2),
-        IdentityEffect(), 
-        ButterworthFilterEffect(440, "lowpass", 2),
-        IdentityEffect(), 
-        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
-    ]
-
     # Create the window and initialize the window norm
-    window = np.hanning(grains[0]["length"])
+    window = window_fn(grains[0]["length"])
     window_norm = window.copy()
 
     grains = grains * n
@@ -104,9 +85,11 @@ def assemble_stochastic(grains: list, n: int, interval: int, rng: random.Random,
         rng.shuffle(grains)
 
     # Apply effects
-    for effect in effect_chain:
-        grains[0]["grain"] = effect(grains[0]["grain"])
-    grains[0]["grain"] = effect_cycle[0](grains[0]["grain"])
+    if effect_chain is not None:
+        for effect in effect_chain:
+            grains[0]["grain"] = effect(grains[0]["grain"])
+    if effect_cycle is not None:
+        grains[0]["grain"] = effect_cycle[0](grains[0]["grain"])
 
     # Add the grain to the audio sequence
     grains[0]["grain"] = operations.adjust_level(grains[0]["grain"], max_db)
@@ -114,11 +97,13 @@ def assemble_stochastic(grains: list, n: int, interval: int, rng: random.Random,
     last_grain_onset = 0
     for i in range(1, len(grains)):
         # Apply effects
-        window = np.hanning(grains[i]["length"])
-        for effect in effect_chain:
-            grains[i]["grain"] = effect(grains[i]["grain"])
-        grains[i]["grain"] = effect_cycle[i % len(effect_cycle)](grains[i]["grain"])
+        if effect_chain is not None:
+            for effect in effect_chain:
+                grains[i]["grain"] = effect(grains[i]["grain"])
+        if effect_cycle is not None:
+            grains[i]["grain"] = effect_cycle[i % len(effect_cycle)](grains[i]["grain"])
         grains[i]["grain"] = operations.adjust_level(grains[i]["grain"], max_db)
+        window = window_fn(grains[i]["length"])
         grains[i]["grain"] *= window
 
         # Add the grain to the audio sequence and update the window norm
@@ -133,60 +118,62 @@ def assemble_stochastic(grains: list, n: int, interval: int, rng: random.Random,
     return final_audio
 
 
-def repeat(grain: dict, n: int, interval: int, max_db: float = -18.0) -> np.ndarray:
+def repeat(grain, n: int, distance_between_grains: int, max_db: float = -18.0, window_fn = np.hanning, effect_chain: list = None, effect_cycle: list = None) -> np.ndarray:
     """
-    Repeats a grain for n times. Applies a window norm to the final assembled grains.
-    :param grain: A grain dictionary
+    Repeats a grain or list of grains for n times. Applies a window norm to the final assembled grains.
+    :param grain: A grain dictionary or list of grains
     :param n: The number of times to repeat
-    :param interval: The interval between each grain, in frames
+    :param distance_between_grains: The distance between each grain, in frames. If negative, grains will overlap. If positive, there will be a gap between grains.
     :param max_db: The db level of the final grains
+    :param window_fn: A window function to apply to all grains
+    :param effect_chain: A sequence (list) of effects that will be applied to each grain individually. If None, no effects will be applied.
+    :param effect_cycle: A cycle (list) of effects that will be applied to each grain in a repeating sequence. If None, no effects will be applied.
     :return: The assembled grains as an array
     """
-    effect_chain = [
-        ButterworthFilterEffect(50, "highpass", 4)
-    ]
-    effect_cycle = [
-        IdentityEffect(), 
-        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
-        IdentityEffect(), 
-        ButterworthFilterEffect(440, "lowpass", 2),
-        IdentityEffect(), 
-        ButterworthFilterEffect(440, "lowpass", 2),
-        IdentityEffect(), 
-        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
-    ]
-
-    grains = [grain] * n
+    if type(grain) == dict:
+        grains = [grain] * n
+    elif type(grain) == list:
+        grains = grain * n
 
     # Create the window and initialize the window norm
-    window = np.hanning(grains[0]["length"])
+    window = window_fn(grains[0]["length"])
     window_norm = window.copy()
 
     # Apply effects
-    for effect in effect_chain:
-        grains[0]["grain"] = effect(grains[0]["grain"])
-    grains[0]["grain"] = effect_cycle[0](grains[0]["grain"])
+    if effect_chain is not None:
+        for effect in effect_chain:
+            grains[0]["grain"] = effect(grains[0]["grain"])
+    if effect_cycle is not None:
+        grains[0]["grain"] = effect_cycle[0](grains[0]["grain"])
 
     # Add the grain to the audio sequence
     grains[0]["grain"] = operations.adjust_level(grains[0]["grain"], max_db)
     final_audio = grains[0]["grain"] * window
-    last_grain_onset = 0
     for i in range(1, len(grains)):
         # Apply effects
-        window = np.hanning(grains[i]["length"])
-        for effect in effect_chain:
-            grains[i]["grain"] = effect(grains[i]["grain"])
-        grains[i]["grain"] = effect_cycle[i % len(effect_cycle)](grains[i]["grain"])
+        if effect_chain is not None:
+            for effect in effect_chain:
+                grains[i]["grain"] = effect(grains[i]["grain"])
+        if effect_cycle is not None:
+            grains[i]["grain"] = effect_cycle[i % len(effect_cycle)](grains[i]["grain"])
         grains[i]["grain"] = operations.adjust_level(grains[i]["grain"], max_db)
+        window = window_fn(grains[i]["length"])
         grains[i]["grain"] *= window
 
         # Add the grain to the audio sequence and update the window norm
-        overlap_num = final_audio.shape[-1] - last_grain_onset - interval
-        new_grain_onset = last_grain_onset + interval
-        final_audio = np.hstack((final_audio[:new_grain_onset], final_audio[new_grain_onset:] + grains[i]["grain"][:overlap_num], grains[i]["grain"][overlap_num:]))
-        window_norm = np.hstack((window_norm[:new_grain_onset], window_norm[new_grain_onset:] + window[:overlap_num], window[overlap_num:]))
-        last_grain_onset = new_grain_onset
+        if distance_between_grains > 0:
+            zeros_shape = list(grains[i]["grain"].shape)
+            zeros_shape[-1] = distance_between_grains
+            final_audio = np.hstack((final_audio, np.zeros(zeros_shape), grains[i]["grain"]))
+        else:
+            final_audio = np.hstack((final_audio[:final_audio.shape[-1] + distance_between_grains], 
+                                     final_audio[final_audio.shape[-1] + distance_between_grains:] + grains[i]["grain"][:distance_between_grains * -1], 
+                                     grains[i]["grain"][distance_between_grains * -1:]))
+            window_norm = np.hstack((window_norm[:window_norm.shape[-1] + distance_between_grains], 
+                                     window_norm[window_norm.shape[-1] + distance_between_grains:] + window[:distance_between_grains * -1], 
+                                     window[distance_between_grains * -1:]))
     
-    final_audio /= window_norm
+    if distance_between_grains < 0:
+        final_audio /= window_norm
     final_audio = np.nan_to_num(final_audio)
     return final_audio
--- granulation/granulator_dev.py
+++ granulation/granulator_dev.py
@@ -18,6 +18,20 @@ if __name__ == "__main__":
     rng = random.Random()
     rng.seed()
     
+    effect_chain = [
+        ButterworthFilterEffect(50, "highpass", 4)
+    ]
+    effect_cycle = [
+        IdentityEffect(), 
+        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
+        IdentityEffect(), 
+        ButterworthFilterEffect(440, "lowpass", 2),
+        IdentityEffect(), 
+        ButterworthFilterEffect(440, "lowpass", 2),
+        IdentityEffect(), 
+        ChorusEffect(2, 0.5, 20, 0.4, 0.5),
+    ]
+
     # The directory containing the files that were analyzed. We can search in here for the files,
     # even if the path doesn't match exactly. This is needed because we may have performed
     # the analysis on a different computer.
@@ -30,29 +44,41 @@ if __name__ == "__main__":
         FROM grains 
         WHERE 
             (spectral_flatness < 0.1) 
-            AND (length = 4096)
+            AND (length = 8192)
             AND (spectral_roll_off_75 < 500)
             AND (energy > 0.2);
     """
+
+    # Retrieve grain metadata and grains
     db, cursor = grain_sql.connect_to_db(DB_FILE)
     grains1 = grain_sql.realize_grains(cursor, SELECT, SOURCE_DIR)
-    if len(grains1) > 1000:
-        grains1 = grains1[:1000]
     print(f"{len(grains1)} grains found.")
-    audio = audiofile.AudioFile(sample_rate=44100, bits_per_sample=24, num_channels=1)
-    # samples = assemble_single(grains1, ["spectral_roll_off_50", "spectral_slope_0_1_khz"], 3000)
-    # samples = grain_assembler.assemble_stochastic(grains1, 5, 3000, rng)
-    samples = grain_assembler.repeat(grains1[rng.randrange(0, len(grains1))], 1000, 500)
-    
-    lpf = signal.butter(2, 500, btype="lowpass", output="sos", fs=44100)
-    hpf = signal.butter(8, 100, btype="highpass", output="sos", fs=44100)
-    samples = signal.sosfilt(lpf, samples)
-    samples = signal.sosfilt(hpf, samples)
-    samples = operations.fade_in(samples, "hanning", 22050)
-    samples = operations.fade_out(samples, "hanning", 22050)
-    samples = operations.adjust_level(samples, -12)
-    audio.samples = samples
-
-    audiofile.write_with_pedalboard(audio, "D:\\Recording\\temp6.wav")
     db.close()
- 
+
+    # Assemble grains
+    # samples = assemble_single(grains1, ["spectral_roll_off_50", "spectral_slope_0_1_khz"], 3000, -18.0, np.hanning, effect_chain, effect_cycle)
+    # samples = grain_assembler.assemble_stochastic(grains1, 5, 3000, rng, -18.0, np.hanning, effect_chain, effect_cycle)
+
+    # Generate candidate audio
+    NUM_CANDIDATES = 5
+    for i in range(NUM_CANDIDATES):
+        LIST_LENGTH = 10
+        grain_list = []
+        for j in range(LIST_LENGTH):
+            grain_list.append(grains1[rng.randrange(0, len(grains1))])
+
+        samples = grain_assembler.repeat(grain_list, 50, -7000, -18.0, np.hanning, effect_chain, effect_cycle)
+        
+        # Apply final effects to the assembled audio
+        lpf = signal.butter(2, 500, btype="lowpass", output="sos", fs=44100)
+        hpf = signal.butter(8, 100, btype="highpass", output="sos", fs=44100)
+        samples = signal.sosfilt(lpf, samples)
+        samples = signal.sosfilt(hpf, samples)
+        samples = operations.fade_in(samples, "hanning", 22050)
+        samples = operations.fade_out(samples, "hanning", 22050)
+        samples = operations.adjust_level(samples, -12)
+
+        # Write the audio
+        audio = audiofile.AudioFile(sample_rate=44100, bits_per_sample=24, num_channels=1)
+        audio.samples = samples
+        audiofile.write_with_pedalboard(audio, f"D:\\Recording\\temp6_{i+1}.wav")
